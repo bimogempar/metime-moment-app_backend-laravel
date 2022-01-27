e498f8bf966fd9584d0732c2d5976108e55cba5c
bimogempar <bimogempar@gmail.com>
2022-01-28 02:18:51 +0700

    add route for hello world

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function () {
+    return 'hello world';
+});
+
 Route::get('/test', function () {
     // project to user
     // $project = Project::find(4);
