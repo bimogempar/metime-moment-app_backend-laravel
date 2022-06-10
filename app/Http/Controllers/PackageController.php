2a222f533c1dc8f31d9b837f2e8e8ba73248d016
bimogempar <bimogempar@gmail.com>
2022-06-10 09:03:22 +0700

    build : new endpoint for package and packagelist models

---
+++ app/Http/Controllers/PackageController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Package;
+use Exception;
+use Illuminate\Http\Request;
+
+class PackageController extends Controller
+{
+    public function getPackages()
+    {
+        try {
+            $packages = Package::with('package_list')->get();
+            return response()->json([
+                'message' => 'success', 'packages' => $packages
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function showPackage($id)
+    {
+        try {
+            $package = Package::with('package_list')->findOrFail($id);
+            return response()->json([
+                'message' => 'success',
+                'package' => $package->with('package_list')->get()
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function storePackage(Request $request)
+    {
+        try {
+            $package = Package::create([
+                'name' => $request->name,
+                'price' => $request->price,
+            ]);
+
+            $package->package_list()->createMany($request->package_list);
+
+            return response()->json([
+                'message' => 'success',
+                'package' => $package->with('package_list')->get()
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function updatePackage(Request $request, $id)
+    {
+        try {
+            $package = Package::find($id);
+            $package->update([
+                'name' => $request->name,
+                'price' => $request->price,
+            ]);
+
+            $package->package_list()->delete();
+            $package->package_list()->createMany($request->package_list);
+
+            return response()->json([
+                'message' => 'success',
+                'package' => $package->with('package_list')->get()
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function destroyPackage($id)
+    {
+        try {
+            $package = Package::find($id);
+            $package->package_list()->delete();
+            $package->delete();
+
+            return response()->json([
+                'message' => 'Deleted Successfully',
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+}
