70835ff3bbfd112ba3f1ea19e29350797504bbb1
bimogempar <bimogempar@gmail.com>
2022-06-11 21:06:54 +0700

    ci : done fix endpoint for endpoint update package

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -11,7 +11,12 @@ class PackageController extends Controller
     public function getPackages()
     {
         try {
-            $packages = Package::with('package_list')->get();
+            $packages = Package::with('package_list')->latest();
+
+            // pagination packages
+            $perPage = request()->has('page') ? request()->page : 10;
+            $packages = $packages->paginate($perPage);
+
             return response()->json([
                 'message' => 'success', 'packages' => $packages
             ], 200);
@@ -64,18 +69,19 @@ class PackageController extends Controller
     public function updatePackage(Request $request, $id)
     {
         try {
-            $package = Package::find($id);
-            $package->update([
+            $findPackage = Package::with('package_list')->find($id);
+            $findPackage->update([
                 'name' => $request->name,
                 'price' => $request->price,
             ]);
 
-            $package->package_list()->delete();
-            $package->package_list()->createMany($request->package_list);
+            $findPackage->package_list()->delete();
+            $findPackage->package_list()->createMany($request->package_list);
+            $package = $findPackage->with('package_list')->find($id);
 
             return response()->json([
                 'message' => 'success',
-                'package' => $package->with('package_list')->get()
+                'package' => $package
             ], 200);
         } catch (Exception $e) {
             return response()->json([
--- app/Models/PackageList.php
+++ app/Models/PackageList.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class PackageList extends Model
 {
     use HasFactory;
+    protected $guarded = [];
 
     public function package()
     {
