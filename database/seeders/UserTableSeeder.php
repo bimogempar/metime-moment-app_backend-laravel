deaba1e7dc041c16797b92485c592dc7aecf3c65
bimogempar <bimogempar@gmail.com>
2022-07-17 11:37:45 +0700

    change user seeder

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -20,18 +20,17 @@ class UserTableSeeder extends Seeder
     public function run()
     {
 
-        User::factory()->count(4)->create()->each(function ($user) {
-            $tokeninitialpassword = Str::random(30);
-            $user->TokenInitialPassword()->save(
-                new TokenInitialPassword([
-                    'token_initial_password' => $tokeninitialpassword,
-                    'status' => 0,
-                ])
-            );
-            // $user->sendEmailRegister($user, $tokeninitialpassword);
-        });
+        // User::factory()->count(4)->create()->each(function ($user) {
+        //     $tokeninitialpassword = Str::random(30);
+        //     $user->TokenInitialPassword()->save(
+        //         new TokenInitialPassword([
+        //             'token_initial_password' => $tokeninitialpassword,
+        //             'status' => 0,
+        //         ])
+        //     );
+        // $user->sendEmailRegister($user, $tokeninitialpassword);
+        // });
 
-        $tokeninitialpasswordAdmin = Str::random(30);
         User::create([
             'name' => 'Admin',
             'username' => 'admin',
@@ -41,26 +40,70 @@ class UserTableSeeder extends Seeder
             'password' => Hash::make('password'),
         ])->TokenInitialPassword()->save(
             new TokenInitialPassword([
-                'token_initial_password' => $tokeninitialpasswordAdmin,
+                'token_initial_password' => Str::random(30),
                 'status' => 1,
             ])
         );
         // )->sendemailregister('admin@example.com', $tokeninitialpassword);
 
-        $tokeninitialpasswordBimo = Str::random(30);
         User::create([
             'name' => 'Bimo Gempar Buono',
             'username' => 'bimogempar',
             'email' => 'bimo@example.com',
+            'no_hp' => '08819417402',
+            'role' => 2,
+            'password' => Hash::make('password'),
+        ])->TokenInitialPassword()->save(
+            new TokenInitialPassword([
+                'token_initial_password' => Str::random(30),
+                'status' => 1,
+            ])
+        );
+        // )->sendemailregister('bimo@example.com', $tokeninitialpassword);
+
+        User::create([
+            'name' => 'Asyraf Shalahudin',
+            'username' => 'asyraf',
+            'email' => 'asyraf@example.com',
+            'no_hp' => '085606986725',
+            'role' => 2,
+            'password' => Hash::make('password'),
+        ])->TokenInitialPassword()->save(
+            new TokenInitialPassword([
+                'token_initial_password' => Str::random(30),
+                'status' => 1,
+            ])
+        );
+        // )->sendemailregister('asyraf@example.com', $tokeninitialpassword);
+
+        User::create([
+            'name' => 'Firly Yoesfi',
+            'username' => 'firly',
+            'email' => 'firly@example.com',
             'no_hp' => '1234567891011',
             'role' => 2,
             'password' => Hash::make('password'),
         ])->TokenInitialPassword()->save(
             new TokenInitialPassword([
-                'token_initial_password' => $tokeninitialpasswordBimo,
+                'token_initial_password' => Str::random(30),
                 'status' => 1,
             ])
         );
-        // )->sendemailregister('bimo@example.com', $tokeninitialpassword);
+        // )->sendemailregister('firly@example.com', $tokeninitialpassword);
+
+        User::create([
+            'name' => 'Ade Novan Guliano',
+            'username' => 'adenovan',
+            'email' => 'adenovan@example.com',
+            'no_hp' => '085607612503',
+            'role' => 2,
+            'password' => Hash::make('password'),
+        ])->TokenInitialPassword()->save(
+            new TokenInitialPassword([
+                'token_initial_password' => Str::random(30),
+                'status' => 1,
+            ])
+        );
+        // )->sendemailregister('firly@example.com', $tokeninitialpassword);
     }
 }
